0247c642cd3bd824fbb8668395895b6d3c9d3838
chektien <chek@gamesstudio.org>
2021-01-22 17:58:52 +0800

    edited tmux.conf

---
+++ setup-ssd.md
@@ -0,0 +1,33 @@
+# check which file is the new drive mapped to
+sudo fdisk -l
+
+# use fdisk to edit partition table and create a new one
+sudo fdisk /dev/sda
+n, p, l, ENTER, ENTER, w
+
+# format the partition using ext4
+sudo mkfs -t ext4 /dev/sda1
+ENTER
+
+# create a dir as the filesystem mount point
+sudo mkdir /owldata
+
+# mount the partition
+sudo mount -t auto /dev/sda1 /owldata
+
+# edit fstab to mount on boot
+sudo vim /etc/fstab
+
+add /dev/sda1 /ssd auto noatime 0 0`
+
+# edit /etc/samba/smb.conf
+add 
+
+[owldata]
+comment = Share
+path = "/ssd"
+writeable = yes
+guest ok = yes
+create mask = 0777
+directory mask = 0777
+force user = pi
--- setup-tensorflow-armv7l.sh
+++ setup-tensorflow-armv7l.sh
@@ -21,24 +21,17 @@ sudo apt install liblapack-dev cython
 sudo pip3 install --upgrade setuptools
 sudo pip3 install pybind11
 sudo pip3 install h5py
+sudo pip3 install gdown
 
-# for arm64
-sudo pip3 install cython==0.29.21
-
-# install gdrive downloader
+# armv71 download and install TensorFlow 2.2.0 wheel
 gdown https://drive.google.com/uc?id=11mujzVaFqa7R1_lB7q0kVPW22Ol51MPg
 pip install tensorflow-2.2.0-cp37-cp37m-linux_armv7l.whl
 
-# armv71 download and install TensorFlow 2.1.0 wheel
-# wget https://github.com/Qengineering/Tensorflow-Raspberry-Pi/raw/master/tensorflow-2.1.0-cp37-cp37m-linux_armv7l.whl
-# pip install tensorflow-2.1.0-cp37-cp37m-linux_armv7l.whl
-
-# armv71 download and install TensorFlow 2.2.0 wheel
-#gdown https://drive.google.com/uc?id=11mujzVaFqa7R1_lB7q0kVPW22Ol51MPg
-#pip install tensorflow-2.2.0-cp37-cp37m-linux_armv7l.whl
+# for arm64
+#sudo pip3 install cython==0.29.21
 
 # arm64 download and install TensorFlow 2.3.0 wheel
-sudo -H pip3 install tensorflow-2.3.0-cp37-cp37m-linux_aarch64.whl
+#sudo -H pip3 install tensorflow-2.3.0-cp37-cp37m-linux_aarch64.whl
 
 # and complete the installation by rebooting
 #sudo reboot
